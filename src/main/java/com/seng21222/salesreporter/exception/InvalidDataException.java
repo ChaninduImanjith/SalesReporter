0f946be1d8b6ace557752293340a4e7f8072da0d
chaninduimanjith <chanindu.imanjith@gmail.com>
2026-09-10 13:44:48 +0530

    Add InvalidDataException

---
+++ src/main/java/com/seng21222/salesreporter/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package com.seng21222.salesreporter.exception;
+
+public class InvalidDataException extends Exception {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
